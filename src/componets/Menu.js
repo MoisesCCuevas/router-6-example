6b35cee39d8f9799032cf3ad06b614185973c255
Moisés C. Cuevas <moisesccuevas@gmail.com>
2023-06-08 17:36:15 -0600

    Some Updates

---
+++ src/componets/Menu.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const routes = [
+  { to: '/', text: "Home"},
+  { to: '/blog', text: "Blog"},
+  { to: '/profile', text: "Profile"}
+];
+
+const Menu = () => {
+  return (
+    <nav>
+      <ul>
+        {/* <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/blog">Blog</Link>
+        </li>
+        <li>
+          <Link to="/profile">Profile</Link>
+        </li> */}
+        {routes.map(({ to, text }) => (
+          <li>
+            <NavLink
+              to={to}
+              //className={({ isActive }) => ''}
+              style={
+                ({ isActive }) => ({ color: isActive ? 'red' : 'blue'})
+              }
+            >
+              {text}
+            </NavLink>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+}
+
+export default Menu;
